8832044ec7d029731822c380deb4de5f5aec2d7a
dabojimenez <dabojimenez16@hotmail.com>
2024-08-04 01:45:48 -0500

    se crea componente sin el CLI de angular

---
+++ src/app/counter/counter.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'app-counter', // se aconseja colcoar un prefijo, en este caso es app
+  template: '<h1>Hola mundo contador</h1>'
+})
+export class CounterComponent {
+
+}
