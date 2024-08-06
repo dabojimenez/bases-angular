0dd1d8489ead554dc8119444cfd283f25187bb9b
dabojimenez <dabojimenez16@hotmail.com>
2024-08-05 21:27:43 -0500

    One way data binding - enlazado en una sola via

--- src/app/heroes/hero/hero.component.ts
+++ src/app/heroes/hero/hero.component.ts
@@ -10,5 +10,24 @@ export class HeroComponent {
   public name: string = 'ironman';
   public age: number = 45;
 
+  // un (get) es un metodo de acceso aun que puede ser considerado como una propiedad
+  get capitalizedName(): string {
+    return this.name.toUpperCase();
+  }
+
+  // (getHeroDescription): este es un metodo y al invocar lo debemos hacer con parentesis
+  getHeroDescription(): string {
+    return `${this.name} - ${this.age}`
+  }
+
+  changeHero(): void {
+    // //TODO:
+    // throw 'Metodo no implementado';
+    this.name = 'Stalyn Hero';
+  }
+
+  changeAge(): void {
+    this.age = 27;
+  }
 
 }
