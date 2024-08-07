218329512459a0289ce4c84b19dcc7849613cacf
dabojimenez <dabojimenez16@hotmail.com>
2024-08-06 20:37:11 -0500

    @input - recibir del padre

--- src/app/dbz/components/list/list.component.ts
+++ src/app/dbz/components/list/list.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Character } from '../../interfaces/character.interface';
 
 @Component({
   selector: 'dbz-list',
@@ -7,4 +8,7 @@ import { Component } from '@angular/core';
 })
 export class ListComponent {
 
+  // este decorador (Input), nos permite obtener valores desde el padre, con el nombre [characterList]
+  @Input()
+  public characterList: Character[] = [];
 }
