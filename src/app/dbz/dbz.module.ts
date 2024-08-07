328db867a8fa6c04b7a1ec359d37b42789abec42
dabojimenez <dabojimenez16@hotmail.com>
2024-08-06 20:25:07 -0500

    crar componentes mas pequenios - responsabilidad unica

--- src/app/dbz/dbz.module.ts
+++ src/app/dbz/dbz.module.ts
@@ -3,12 +3,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { MainPageComponent } from './pages/main-page.component';
+import { ListComponent } from './components/list/list.component';
+import { AddCharacterComponent } from './components/add-character/add-character.component';
 
 
 @NgModule({
   declarations: [
     // colocamos los componnetes que vamos a usar en este modulo
-    MainPageComponent
+    MainPageComponent,
+    ListComponent,
+    AddCharacterComponent
   ],
   exports: [
     // exportamos los componentes que queremso que se vean desde otro modulo
